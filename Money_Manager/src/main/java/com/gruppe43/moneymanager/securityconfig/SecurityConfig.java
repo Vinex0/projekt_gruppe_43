4cfdadb7498e49ad715c8c20629a2906eff7bc98
AkiraTenchi <jimjan2002@gmail.com>
2023-03-09 10:21:43 +0100

    enabled Post mappings

--- Money_Manager/src/main/java/com/gruppe43/moneymanager/securityconfig/SecurityConfig.java
+++ Money_Manager/src/main/java/com/gruppe43/moneymanager/securityconfig/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.gruppe43.moneymanager.securityconfig;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -12,10 +17,12 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain configure(HttpSecurity chainBuilder) throws Exception {
 
+    chainBuilder.cors().and().csrf().disable();
+
     chainBuilder.authorizeHttpRequests(
         configurer -> configurer
-            .requestMatchers("/", "/css/*").permitAll()
-            .anyRequest().authenticated()
+            .requestMatchers("*", "/", "/css/*").permitAll().requestMatchers(HttpMethod.POST, "*").permitAll()
+            .anyRequest().permitAll()
     ).httpBasic(Customizer.withDefaults());
 //        .oauth2Login(config ->
 //            config.userInfoEndpoint(
@@ -24,6 +31,5 @@ public class SecurityConfig {
 
     return chainBuilder.build();
   }
-
 
 }
--- Money_Manager/src/main/java/com/gruppe43/moneymanager/web/ApiController.java
+++ Money_Manager/src/main/java/com/gruppe43/moneymanager/web/ApiController.java
@@ -3,13 +3,19 @@ package com.gruppe43.moneymanager.web;
 import com.gruppe43.moneymanager.domain.Gruppe;
 import com.gruppe43.moneymanager.service.GruppenService;
 import com.gruppe43.moneymanager.service.Serializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class ApiController {
+
   private final GruppenService gruppenService;
 
   public ApiController(GruppenService gruppenService) {
@@ -17,9 +23,9 @@ public class ApiController {
   }
 
   @GetMapping("/api/gruppen/{id}")
-      public String getGruppe(@PathVariable("id") int id) {
-      Gruppe gruppe = gruppenService.getGroupbyID(id);
-      return Serializer.gruppeToJson(gruppe);
+  public String getGruppe(@PathVariable("id") int id) {
+    Gruppe gruppe = gruppenService.getGroupbyID(id);
+    return Serializer.gruppeToJson(gruppe);
   }
 
   @GetMapping("/api/gruppen/{id}/ausgleich")
@@ -32,17 +38,25 @@ public class ApiController {
     return Serializer.gruppenListToJson(gruppenService.getGruppenByNutzer(name));
   }
 
- /* @PostMapping("/gruppen")
-    public String createGruppe() {
+  @PostMapping("/gruppen")
+  public String createGruppe() {
+    return null;
 
   }
-  @PostMapping("/api/gruppen/{id}/schliesen")
-  public String closeGruppe() {
 
+  @PostMapping("/api/gruppen/{id}/schliessen")
+  public ResponseEntity<?> closeGruppe(@PathVariable("id") int id) {
+    if (gruppenService.getGroupbyID(id) != null){
+      //todo
+      return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
   }
+
   @PostMapping("/api/gruppen/{id}/auslagen")
   public String createAusgabe() {
+    return null;
 
   }
-  */
+
 }
