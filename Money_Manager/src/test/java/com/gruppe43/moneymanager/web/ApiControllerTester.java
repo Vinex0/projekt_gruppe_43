e1941c4e70230a8b2a9a1f05712f4df23950c472
MoritzOh <moritz.ohrt@gmail.com>
2023-03-14 10:42:10 +0100

    Revert "Revert "api controller tests""
    This reverts commit a43fb1063f4586fb72b1e6c82b753f7a9864a1c8.

--- Money_Manager/src/test/java/com/gruppe43/moneymanager/web/ApiControllerTester.java
+++ Money_Manager/src/test/java/com/gruppe43/moneymanager/web/ApiControllerTester.java
@@ -1,7 +1,9 @@
 package com.gruppe43.moneymanager.web;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import com.gruppe43.moneymanager.domain.Ausgabe;
 import com.gruppe43.moneymanager.domain.Gruppe;
 import com.gruppe43.moneymanager.helper.WithMockOAuth2User;
 import com.gruppe43.moneymanager.service.CheckboxHelper;
@@ -46,15 +48,63 @@ public class ApiControllerTester {
         .andExpect(status().isNotFound());
   }
 
-/*  @Test
+  @Test
   void getGruppeTest() throws Exception {
     Gruppe testGruppe = new Gruppe("TestGruppe", "Peter");
+    testGruppe.addTeilnehmer("Jens");
+    testGruppe.createAusgabe("Peter",List.of("Jens"), Money.of(10, "EUR"), "TestAusgabe");
+    String expectedResponse = "{\"gruppe\" : \"null\", \"name\" : \"TestGruppe\", \"personen\" : [\"Peter\", \"Jens\"], \"geschlossen\" : false, \"ausgaben\" : [{\"grund\" : \"TestAusgabe\", \"glaeubiger\" : \"Peter\", \"cent\" : 1000, \"schuldner\" : [\"Jens\"]}]}";
     when(gruppenService.getGruppeById("0")).thenReturn(testGruppe);
     mvc.perform(get("/api/gruppen/0")
         .param("id", "0"))
         .andExpect(status().isOk())
-        .andExpect(content().string(Serializer.gruppeToJson(testGruppe)));
-  }*/
+        .andExpect(content().string(expectedResponse));
+  }
+
+  @Test
+  void getAusgleichTest() throws Exception {
+    Gruppe testGruppe = new Gruppe("TestGruppe", "Peter");
+    testGruppe.addTeilnehmer("Jens");
+    testGruppe.createAusgabe("Peter",List.of("Jens"), Money.of(10, "EUR"), "TestAusgabe");
+    when(gruppenService.getGruppeById("0")).thenReturn(testGruppe);
+    String expectedResponse = "[{\"von\" : \"Jens\", \"an\" : \"Peter\", \"cents\" : 1000}]";
+    mvc.perform(get("/api/gruppen/0/ausgleich")
+        .param("id", "0"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(expectedResponse));
+  }
+
+  @Test
+  void getGruppenByUserTest() throws Exception {
+    Gruppe testGruppe = new Gruppe("TestGruppe", "Peter");
+    testGruppe.addTeilnehmer("Jens");
+    when(gruppenService.getGruppenByNutzer("Peter")).thenReturn(List.of(testGruppe));
+    String expectedResponse = "[{\"gruppe\" : \"null\", \"name\" : \"TestGruppe\", \"personen\" : [\"Peter\", \"Jens\"]}]";
+    mvc.perform(get("/api/user/Peter/gruppen")
+        .param("name", "Peter"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(expectedResponse));
+
+  }
+
+  @Test
+  void createGruppeBadInputTest() throws Exception {
+    mvc.perform(post("/api/gruppen")
+        .param("data", "ciasdhodvhndiobnh"))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void createGruppeTest() throws Exception {
+    String body = "{\"name\" : \"Test Gruppe\", \"personen\" : [\"Peter\", \"Jens\", \"Moritz\"] }";
+    when(gruppenService.getGruppe(anyString())).thenReturn(mock(Gruppe.class));
+    when(gruppenService.getGruppe(anyString()).getId()).thenReturn("0");
+    mvc.perform(post("/api/gruppen")
+        .content(body))
+        .andExpect(status().isCreated())
+        .andExpect(content().string("0"));
+    verify(gruppenService).addGruppe("Test Gruppe", "Peter");
+  }
 
 @Test
 void gruppeSchliessenTest() throws Exception{
@@ -66,16 +116,19 @@ void gruppeSchliessenTest() throws Exception{
   assertThat(testGruppe.isClosed()).isTrue();
 }
 
-  /*@Test
+  @Test
+
   void createAusgabeTest() throws Exception {
+    String body = "{\"grund\": \"IntelliJ Ultimate Abo\", \"glaeubiger\": \"Peter\", \"cent\" : 5000, \"schuldner\" : [\"Jens\"]}";
     Gruppe testGruppe = new Gruppe("TestGruppe", "Peter");
-    String requestBody = "{\"grund\": \"IntelliJ Ultimate Abo\", \"glaeubiger\": \"Peter\", \"cent\" : 5000, \"schuldner\" : [\"Peter\", \"Jens\"]}";
+    testGruppe.addTeilnehmer("Jens");
     when(gruppenService.getGruppeById("0")).thenReturn(testGruppe);
     mvc.perform(post("/api/gruppen/0/auslagen")
         .param("id", "0")
-        .param("data", ""))
+        .content(body))
         .andExpect(status().isCreated());
-  }*/
+  }
+
 
 
 }
