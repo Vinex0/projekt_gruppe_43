2be6ce96a2a28b418685400bb7ba8132715cd787
Vinex0 <33190118+Vinex0@users.noreply.github.com>
2023-03-15 11:20:43 +0100

    finished migration script

---
+++ Money_Manager/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,38 @@
+create table if not exists gruppe(
+    id serial primary key ,
+    titel varchar(150)  ,
+    start_person varchar(100)  ,
+    geschlossen boolean
+);
+create table if not exists teilnehmer(
+    id serial primary key ,
+    foreign key (id) references gruppe(id),
+    person varchar(100)
+);
+
+create table if not exists gruppen_teilnehmer(
+    gruppe integer references gruppe(id),
+    teilnehmer integer references teilnehmer(id)
+);
+
+create table if not exists ausgabe(
+    id serial primary key ,
+    gruppe integer references gruppe(id),
+    titel varchar(100),
+    summe double precision
+);
+
+create table if not exists ausgaben_teilnehmer(
+    ausgabe integer references ausgabe(id),
+    teilnehmer integer references teilnehmer(id)
+);
+
+create table if not exists schulden(
+    gruppe integer references gruppe(id),
+    glaeubiger integer references teilnehmer(id),
+    schuldner integer references teilnehmer(id),
+    summe double precision
+);
+
+
+
