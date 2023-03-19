c30b258f4a2891e0bf4b1592f77ae6292444e782
optecs <63349502+optecs@users.noreply.github.com>
2023-03-19 14:17:54 +0100

    add new persistence tests

--- Money_Manager/src/test/java/com/gruppe43/moneymanager/service/ServiceTest.java
+++ Money_Manager/src/test/java/com/gruppe43/moneymanager/service/ServiceTest.java
@@ -7,7 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import com.gruppe43.moneymanager.database.GruppenRepositoryImpl;
 import com.gruppe43.moneymanager.database.SpringDataGruppenRepository;
 import com.gruppe43.moneymanager.domain.Gruppe;
+import com.gruppe43.moneymanager.exceptions.NichtVorhandenException;
+import java.util.List;
+import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +31,53 @@ public class ServiceTest {
   }
 
   @Test
-  @DisplayName("Eie Gruppe kann hinzugefuegt werden")
+  @Disabled
+  @DisplayName("Gruppe erstellen")
   void test_1() {
     GruppenService service = new GruppenService(gruppenRepository);
     service.gruppeHinzufuegen("titel", "person");
     assertThat(service.getGruppenByNutzer("person")).contains(new Gruppe("titel", "person", 1));
+  }
+
+  @Test
+  @Disabled
+  @DisplayName("Gruppe schliessen")
+  void test_2() {
+    GruppenService service = new GruppenService(gruppenRepository);
+    service.gruppeHinzufuegen("gruppe", "name");
+    service.teilnehmerHinzufuegen(1, "name1");
+    service.ausgabeHinzufuegen(1,"name", List.of("name", "name1"), Money.of(20, "EUR"), "titel");
+    service.close(1);
+    assertThat(service.getGruppeById(1).isClosed()).isTrue();
+  }
+
+  @Test
+  @Disabled
+  @DisplayName("Ausgabe erstellen")
+  void test_3() {
+    GruppenService service = new GruppenService(gruppenRepository);
+    Gruppe gruppe = service.gruppeHinzufuegen("gruppe", "person");
+    gruppe.addTeilnehmer("person1");
+    gruppe.createAusgabe("person", List.of("name", "name1"), Money.of(20, "EUR"), "titel");
+    assertThat(gruppe.getAusgaben()).isNotEmpty();
+  }
+
+  @Test
+  @Disabled
+  @DisplayName("Teilnehmer hinzufuegen")
+  void test_4() {
+    GruppenService service = new GruppenService(gruppenRepository);
+    service.gruppeHinzufuegen("gruppe", "name");
+    service.teilnehmerHinzufuegen(1, "teilnehmer");
+    assertThat(service.getGruppeById(1).getTeilnehmer()).contains("name", "teilnehmer");
+  }
+
+  @Test
+  @Disabled
+  @DisplayName("Gruppe nicht vorhanden")
+  void test_5() {
+    GruppenService service = new GruppenService(gruppenRepository);
+    assertThat(service.alleGruppen()).isEmpty();
+    assertThrows(NichtVorhandenException.class, ()-> service.getGruppeById(20));
   }
 }
